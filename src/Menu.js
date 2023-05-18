459a7c6f9a9400a3f3aaec07dfd39044682945eb
Michael <109005544+NoMad100@users.noreply.github.com>
2023-05-18 11:56:33 -0700

    saving progress. trying to solve basic controls

--- src/Menu.js
+++ src/Menu.js
@@ -2,6 +2,10 @@ class Menu extends Phaser.Scene{
     constructor(){
         super("Menu");
     }
+    preload(){
+        // this.load.image("redball", "../assets/istockphoto-895119304-612x612.jpg");
+    }
     create(){
+        let test = this.physics.add.existing(new Player(this, 400, 400));
     }
 }
--- src/player.js
+++ src/player.js
@@ -1,5 +1,21 @@
-class Player extends Phaser.Physics.Arcade.Body{
-    constructor(){
-        super();
+class Player extends Phaser.Physics.Arcade.Sprite{
+    xPos;
+    yPos;
+    xSpeed;
+    ySpeed;
+    xCharge;
+    yCharge;
+    chargeScale;
+
+    constructor(scene, x, y){
+        super(scene, x, y);
+    }
+    calculateMove(){
+        // if(this.input.keyboard.){
+            ;
+        // }
+    }
+    applyXForce(velocityx){
+        ;
     }
 }
