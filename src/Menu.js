df1e76a1c3f34a0a86ae3f94d4975dae58b17aaf
Michael <109005544+NoMad100@users.noreply.github.com>
2023-05-28 20:42:20 -0700

    Added y axis mvment. Solved drag issue.

--- src/Menu.js
+++ src/Menu.js
@@ -9,23 +9,37 @@ class Menu extends Phaser.Scene{
         let xbuffer = 0;
         let ybuffer = 0;
 
+        // conditionals for checking if a key is already pressed.
+        // I know there are probably better ways but im prototyping fast here so whatever
+        let isRight = false;
+        let isLeft = false;
+        let isUp = false;
+        let isDown = false;
+
         // player ball
         let ball = this.add.circle(100, 100, 20, 0xff0000);
         this.physics.add.existing(ball);
         ball.body.setCircle(20)
         .setCollideWorldBounds(true)
         .setBounce(1)
-        .setDrag(1000);
-        // ball.body.setVelocity(100, 0);
+        .setDamping(true)
+        .setDrag(0.5);
+        ball.body.setVelocity(30, 20);
 
         // key right movement
         this.input.keyboard.on("keydown-RIGHT", event=>{
-            console.log("Rdown");
-            // if(!this.input.keyboard.LEFT.isDown){
+            isRight = true;
+            if(isLeft){
+                console.log("Left triggered while Right already held, cancelling buffer movement.");
+                xbuffer = 0;
+            }
+            else{
+                console.log("Rdown");
                 xbuffer += 100;
-            // }
+            }
         });
         this.input.keyboard.on("keyup-RIGHT", event=>{
+            isRight = false;
             console.log("Rup, xbuffer = ", xbuffer);
             ball.body.setVelocity(ball.body.velocity.x + xbuffer, ball.body.velocity.y);
             xbuffer = 0;
@@ -33,16 +47,60 @@ class Menu extends Phaser.Scene{
 
         // key left movement
         this.input.keyboard.on("keydown-LEFT", event=>{
-            console.log("Ldown");
-            // if(!this.input.keyboard.RIGHT.isDown){
+            isLeft = true;
+            if(isRight){
+                console.log("Right triggered while Left already held, cancelling buffer movement.");
+                xbuffer = 0;
+            }
+            else{
+                console.log("Ldown");
                 xbuffer-= 100;
-            // }
+            }
         });
         this.input.keyboard.on("keyup-LEFT", event=>{
+            isLeft = false;
             console.log("Lup, xbuffer = ", xbuffer);
             ball.body.setVelocity(ball.body.velocity.x + xbuffer, ball.body.velocity.y);
             xbuffer = 0;
         });
+
+        // key up movement
+        this.input.keyboard.on("keydown-UP", event=>{
+            isUp = true;
+            if(isDown){
+                console.log("Down triggered while Up already held, cancelling buffer movement.");
+                ybuffer = 0;
+            }
+            else{
+                console.log("Udown");
+                ybuffer-=100;
+            }
+        });
+        this.input.keyboard.on("keyup-UP", event=>{
+            isUp = false;
+            console.log("Uup, ybuffer = ", ybuffer);
+            ball.body.setVelocity(ball.body.velocity.x, ball.body.velocity.y+ybuffer);
+            ybuffer=0;
+        });
+        // key down movement
+        this.input.keyboard.on("keydown-DOWN", event=>{
+            isDown = true;
+            if(isUp){
+                console.log("Up triggered while Down already held, cancelling buffer movement.");
+                ybuffer = 0;
+            }
+            else{
+                console.log("Ddown");
+                ybuffer+=100;
+            }
+        });
+        this.input.keyboard.on("keyup-DOWN", event=>{
+            isDown = false;
+            console.log("Dup, ybuffer = ", ybuffer);
+            ball.body.setVelocity(ball.body.velocity.x, ball.body.velocity.y+ybuffer);
+            ybuffer=0;
+        });
+
         // ball.body.drawDebug(new Phaser.GameObjects.Graphics(this));
         // ball.body.x = 500;
         console.log("yvelocity is = ", ball.body.velocity.y);
